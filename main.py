257331e5a7f8249a57397247df179efa744d9553
Mayur Patil <102802362+betheone01@users.noreply.github.com>
2026-05-20 20:19:03 +0530

    bootstrap FastAPI application with development environment setup and HTTP request handling basics

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
-app= FastAPI()
 
-@app.get("/")
+app= FastAPI() #Instantiate Fastapi 
+
+
+# path operation or route
+#"/" is root path 
+@app.get("/") #decorator
 def root():
-    return {"Message":"Healthy"}
+    return {"Message":"Welcome To The FastApi"}
 
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data":"This is the posts"}
+
+
+@app.post("/posts")
+def create_post(payload:dict=Body(...)):
+    print(payload)
+    return {"Message":payload}
